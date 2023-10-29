188348d19a8e49cc9efcd165b81d8c843c64bf49
ggertzog <musienko.roma@mail.ru>
2023-10-29 19:17:49 +0300

    express-mesto-gha

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
+const {ERROR_CODE} = require('../utils/constants')
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res) => {
+  return res.status(ERROR_CODE.NOT_FOUND).send({
+    message: 'Страницы не существует'
+  })
+})
 
 module.exports = router;
