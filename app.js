cc838d04e630a7c27a03a3dc1ef3e1b6b983534b
ggertzog <musienko.roma@mail.ru>
2023-10-30 12:10:42 +0300

    express-mesto-gha/fix-package

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const router = require('./routes');
-const app = express();
+
+
 
 const { PORT = 3000 } = process.env;
 
@@ -10,6 +11,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+const app = express();
 
 app.use(express.json());
 app.use(helmet);
@@ -24,6 +27,4 @@ app.use((req, res, next) => {
 
 app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-});
+app.listen(PORT);
